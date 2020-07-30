8a7e0f140d4669aa32267674098e5988e071520a
Will Vetter <vetter.william@gmail.com>
2020-07-30 08:09:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(array_of_colors)
+  puts array_of_colors[0]
+  puts array_of_colors[1]
+  puts array_of_colors[2]
+  puts array_of_colors[3]
+  puts array_of_colors[4]
+  puts array_of_colors[5]
+  puts array_of_colors[6]
+
+end
