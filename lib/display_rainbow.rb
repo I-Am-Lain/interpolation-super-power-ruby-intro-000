031fb392c7eb1e3ef9329d58cae9e8284869a994
Will Vetter <vetter.william@gmail.com>
2020-07-30 08:15:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(array_of_colors)
-  puts "#{array_of_colors[0][0].capitalize}: #{array_of_colors[0]}"
+  puts "#{array_of_colors[0][0].capitalize}: #{array_of_colors[0]}".chomp
   puts "#{array_of_colors[1][0].capitalize}: #{array_of_colors[1]}"
   puts "#{array_of_colors[2][0].capitalize}: #{array_of_colors[2]}"
   puts "#{array_of_colors[3][0].capitalize}: #{array_of_colors[3]}"
